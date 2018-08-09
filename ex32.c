df3857bfffe2ba3e244448369bfdbf4d99203c2b
ChuOkupai <adrienadrien@neuf.fr>
2018-08-09 08:21:04 +0200

    ajout du TD 6

--- ex32.c
+++ ex32.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	POINT p[10];
-	int i;
+	POINT p[10], user;
+	int i, j;
 	
 	init_graphics(640, 480);
 	for (i = 0; i < 10; i++)
@@ -11,10 +11,20 @@ int main()
 		p[i] = wait_clic();
 		draw_circle(p[i], 10, red);
 	}
-	for (i = 9; i >= 0; i--)
+	i = 0;
+	while (i < 10)
 	{
-		wait_clic();
-		draw_circle(p[i], 10, black);
+		user = wait_clic();
+		for (j = 0; j < 10; j++)
+		{
+			if (((user.x - p[j].x) * (user.x - p[j].x)) + ((user.y - p[j].y) * (user.y - p[j].y)) <= 100)
+			{
+				draw_circle(p[j], 10, black);
+				p[j].x = 650;
+				p[j].y = 490;
+				i++;
+			}
+		}
 	}
 	wait_escape();
 	return 0;
